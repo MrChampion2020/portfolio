eea6963eb9d69d7d12d166db9dd757029cdffa8a
runorx <145400827+runorx@users.noreply.github.com>
2024-06-06 04:57:10 +0100

    merge remote changes

---
+++ server.js
@@ -0,0 +1,188 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const http = require("http").createServer(app);
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const User = require("./models/User");
+
+// Middleware to parse JSON and URL-encoded data
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './')));
+
+//session
+app.use(session({
+  secret: 'yourSecretKey',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+  });
+
+// Generate a secret key for JWT
+const generateSecretKey = () => crypto.randomBytes(32).toString("hex");
+const secretKey = generateSecretKey();
+
+// Send verification email function
+const sendVerificationEmail = async (email, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  const API_URL = process.env.API_URL;
+  const mailOptions = {
+    from: "Elitearn",
+    to: email,
+    subject: "Email Verification",
+    text: `Please click on the following link to verify your email: ${API_URL}/verify/${verificationToken}`,
+    html: `<p style="margin: auto; font-size: 20px; width: 60%; height: 60%;">Please click on the following link to verify your email: <a href="${API_URL}/verify/${verificationToken}">${API_URL}/verify/${verificationToken}</a>VERIFY</p>`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("Error sending the verification email:", error);
+  }
+};
+
+// Registration endpoint
+app.post("/register", async (req, res) => {
+  try {
+    const { fullName, email, phone, password, username } = req.body;
+
+    // Debugging logs
+    console.log("Received registration data:", { fullName, email, phone, password, username });
+
+    if (!username) {
+      return res.status(400).json({ message: "Username is required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already taken" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      fullName,
+      email,
+      phone,
+      password: hashedPassword,
+      username,
+      verificationToken: crypto.randomBytes(20).toString("hex"),
+    });
+
+    await newUser.save();
+    await sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+    res.status(200).json({ message: "User registered successfully", userId: newUser._id });
+  } catch (error) {
+    console.log("Error registering user:", error);
+    if (error.code === 11000) {
+      return res.status(400).json({ message: "Duplicate key error", error: error.message });
+    }
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
+
+// Email verification endpoint
+app.get("/verify/:token", async (req, res) => {
+  try {
+    const token = req.params.token;
+    const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid verification token" });
+    }
+
+    user.verified = true;
+    user.verificationToken = undefined;
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    console.log("Error verifying email:", error);
+    res.status(500).json({ message: "Email verification failed" });
+  }
+});
+
+
+
+// Login endpoint
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !await bcrypt.compare(password, user.password)) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Create a session
+    req.session.user = {
+      userId: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+      phone: user.phone,
+      wallet: user.wallet,
+    };
+
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed", error });
+  }
+});
+
+// Get user details endpoint
+app.get("/user-details", async (req, res) => {
+  try {
+    if (!req.session.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await User.findById(req.session.user.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ user: { name: user.name, email: user.email, username: user.username, phone: user.phone, wallet: user.wallet } });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user details", error });
+  }
+});
+
+
+// Listening to the server
+http.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
