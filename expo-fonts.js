3cfdda629e760b7b448e7ee22672baf90cedfeb6
Champion Aden <65666205+MrChampion2020@users.noreply.github.com>
2024-06-06 04:32:41 +0100

    Delete expo-fonts.js

--- expo-fonts.js
+++
@@ -1,9 +0,0 @@
-import * as Font from 'expo-font';
-
-export const loadFonts = async () => {
-  await Font.loadAsync({
-    Gothic: require('./assets/fonts/Gothic.ttf'),
-    'Gothic-Bold': require('./assets/fonts/Gothic-Bold.ttf'),
-    // Add other variations of the Gothic font if needed
-  });
-};
