69b5999623036b19c6a455eee508b8215882af4e
runorx <145400827+runorx@users.noreply.github.com>
2024-06-10 02:39:12 +0200

    merge remote changes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const secretKey = process.env.SECRET_KEY || generateSecretKey();
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("Error connecting to MongoDB:", error));
 
