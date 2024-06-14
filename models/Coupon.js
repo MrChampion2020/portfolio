b42644f866c606000eb2ac9a7be4b6adec8c381b
Champion Aden <65666205+MrChampion2020@users.noreply.github.com>
2024-06-14 05:31:29 +0100

    Update Coupon.js

--- models/Coupon.js
+++ models/Coupon.js
@@ -1,29 +1,15 @@
 const mongoose = require('mongoose');
 
 const CouponSchema = new mongoose.Schema({
-<<<<<<< HEAD
+
  code: { type: String, required: true, unique: true },
  value: { type: Number, required: true },
  currency: { type: String, required: true },
   vendorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor' },
   status: { type: String, enum: ['active', 'used'], default: 'active' },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-=======
-  code: { type: String, required: true, unique: true },
-  value: { type: Number, required: true },
-  currency: { type: String, required: true },
-  vendorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor' },
-  status: { type: String, enum: ['active', 'used'], default: 'active' },
-   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
->>>>>>> 14e0641296392591aa91815db0f2a139321e6645
-}, { timestamps: true }
-  
-);
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true,  timestamps: true }
+
+});
 
 module.exports = mongoose.model('Coupon', CouponSchema);
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 14e0641296392591aa91815db0f2a139321e6645
