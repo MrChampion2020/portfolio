84ad760f7d12a19452607d3d6db85f65f61a7cd1
runorx <145400827+runorx@users.noreply.github.com>
2024-06-15 09:47:16 +0100

    merge remote changes

--- models/Admin.js
+++ models/Admin.js
@@ -1,10 +1,23 @@
-const mongoose = require('mongoose');
+// Admin schema and 
+const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
-  username: { type: String, default: 'admin' },
-  password: { type: String, default: 'Admin' }
-  // Other admin-specific fields
+  fullName: { type: String, required: true },
+  phone: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+adminSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const Admin = mongoose.model('Admin', adminSchema);
+
 module.exports = Admin;
