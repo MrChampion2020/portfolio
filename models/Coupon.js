478e03e357de54d04ceed8736734442e8af5247b
Champion Aden <65666205+MrChampion2020@users.noreply.github.com>
2024-06-15 06:57:44 +0100

    Update Coupon.js

--- models/Coupon.js
+++ models/Coupon.js
@@ -1,21 +1,3 @@
-/*const mongoose = require('mongoose');
-
-const CouponSchema = new mongoose.Schema({
-
- code: { type: String, required: true, unique: true },
- value: { type: Number, required: true },
- currency: { type: String, required: true },
- userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  vendorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor' },
-  isActive: { type: Boolean, default: true },
-  isUsed: { type: Boolean, default: false },
-  status: { type: String, enum: ['active', 'used'], default: 'active' },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true,  timestamps: true }
-
-});
-module.exports = mongoose.model('Coupon', CouponSchema);
-*/
-
 const mongoose = require("mongoose");
 
 const couponSchema = new mongoose.Schema({
@@ -28,3 +10,5 @@ const couponSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Coupon", couponSchema);
+
+
