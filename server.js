c050453d463dee4ba9d7f055aef56162dfddcd9c
Champion Aden <65666205+MrChampion2020@users.noreply.github.com>
2024-06-19 02:27:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -402,6 +402,30 @@ app.get("/admin-details", authenticateAdminToken, async (req, res) => {
   }
 });
 
+
+// Endpoint to get the number of users
+app.get('/admin/user-count', authenticateAdminToken, async (req, res) => {
+  try {
+    const userCount = await User.countDocuments();
+    res.status(200).json({ userCount });
+  } catch (error) {
+    console.error('Error fetching user count:', error);
+    res.status(500).json({ message: 'Failed to fetch user count' });
+  }
+});
+
+// Endpoint to fetch user details
+app.get('/admin/users', authenticateAdminToken, async (req, res) => {
+  try {
+    const users = await User.find({}, { password: 0 }); // Exclude password field
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ message: 'Failed to fetch users' });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
